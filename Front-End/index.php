97a688b64c6a33b4b977b4fc81e66925eb8dc015
Cristóbal López H <cristobal.lopherr@gmail.com>
2024-11-09 13:50:00 -0300

    Inicio de integración Categorias.

--- Front-End/index.php
+++ Front-End/index.php
@@ -6,6 +6,7 @@
     <title>FukusukeSushi</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <link rel="stylesheet" href="/FukusukeSushi/Front-End/css/styles.css">
 </head>
 
@@ -166,8 +167,12 @@
         </button>
     </div>
 
-    <div class="col-12" style="background-color: red; height: 200px">
-        <div class="col-3" style="background-color: blue; height: 200px">
+    <div class="col-12" style="background-color: grey; height: 200px">
+        <div class="col-3" style="height: 200px">
+            <label for="categoria" class="form-label">Ciudad</label> 
+            <select multiple class="form-select" id="categoria" name="categoria">
+                
+            </select>
 
 
     </div>
