5fedf9e23b6f66a2d7b99f6859264e281c7cdeaf
BobCortesP <bob.cortes@hotmail.com>
2024-11-15 01:52:11 -0300

    Se agrega el carrito y modal de productos al main. Falta actualizar precios en el carrito

---
+++ Front-End/js/carrito.js
@@ -0,0 +1,298 @@
+// Deja listo el carrito
+async function setCarrito(idCliente){
+    const carritosResponse = await obtenerCarritos(idCliente);
+    let idCarrito = carritosResponse.data.getCarritosByIdCliente;
+    if (idCarrito.length === 0) {
+        idCarrito = await crearCarrito(idCliente);
+    } else {
+        idCarrito = idCarrito[0].id;
+    }
+    document.getElementById('carrito').setAttribute('value', idCarrito);
+}
+// Chequea si el cliente tiene asignado algún carrito
+async function obtenerCarritos(idCliente) {
+    const query = `
+        query getCarritosByIdUsuario($getCarritosByIdClienteId: String) {
+            getCarritosByIdCliente(id: $getCarritosByIdClienteId) {
+                id
+                fecha
+            }
+        }
+    `;
+    try {
+        let responseCarrito = await $.ajax({
+            type: "POST",
+            url: "http://localhost:8091/graphql",
+            contentType: "application/json",
+            timeout: 15000,
+            data: JSON.stringify({
+                query,
+                variables: {
+                    getCarritosByIdClienteId: idCliente
+                }
+            })
+        });
+        return responseCarrito;
+    } catch(error){
+        console.log("Error al buscar los carritos del usuario;", error);
+    }
+    return 
+}
+// Crea el carrito si es que el cliente no tiene asignado ninguno
+async function crearCarrito(idCliente) {
+    const mutation = `
+        mutation AddCarrito($input: CarritoInput) {
+            addCarrito(input: $input) {
+                id
+                fecha
+            }
+        }
+    `;
+    
+    try {
+        const responseCarrito = await $.ajax({
+            type: "POST",
+            url: "http://localhost:8091/graphql",
+            contentType: "application/json",
+            timeout: 15000,
+            data: JSON.stringify({
+                query: mutation,
+                variables: {
+                    input: {
+                        fecha: new Date().toISOString(),
+                        cliente: idCliente
+                    }
+                }
+            })
+        });
+        if (responseCarrito.errors) {
+            console.error("Error en la mutación:", responseCarrito.errors);
+        } else {
+            return responseCarrito.data.addCarrito.id;
+        }
+    } catch (error) {
+        console.error("Error en la solicitud al crear el carrito:", error);
+    }
+}
+
+// Funciones que actualizan el carrito
+async function actualizarCarrito(){
+    let idCarrito = document.getElementById('carrito').getAttribute('value');
+    let prodsCarrito = await getProductosByIdCarrito(idCarrito);
+    let cardsCarrito = [];
+    let query =`
+    query QQuery($getProductoByIdId: ID!){
+        getProductoById(id: $getProductoByIdId) {
+            id
+            nombre
+            foto
+        }
+    }
+    `;
+    for(prod of prodsCarrito){
+        let response = await $.ajax({
+            type: "POST",
+            url: "http://localhost:8091/graphql",
+            contentType: "application/json",
+            timeout: 15000,
+            data: JSON.stringify({
+                query: query,
+                variables: {
+                    getProductoByIdId: prod.producto
+                }
+            })
+        });
+        let precio = await GetUltimoPrecioHistoricoByIdProducto(prod.producto);
+        cardsCarrito.push(creaCardCarrito(response.data.getProductoById, prod.cantidad, prod.id, precio));
+    }
+    if (cardsCarrito.length == 0){
+        document.querySelector('#carrito .offcanvas-body').innerHTML = '<h3 class="text-center">Carrito Vacío 😢</h3><h5 class="text-center">Cuando agregues productos al carrito, aparecerán aquí.</h5>'
+    } else {
+        document.querySelector('#carrito .offcanvas-body').innerHTML = cardsCarrito.join("");
+    }
+}
+async function getProductosByIdCarrito(idCarrito) {
+    let query =`
+    query Query($getDetalleCarritosByIdCarritoId: String){
+        getDetalleCarritosByIdCarrito(id: $getDetalleCarritosByIdCarritoId) {
+            id
+            cantidad
+            producto
+        }
+    }
+    `;
+    try {
+        let response = await $.ajax({
+            type: "POST",
+            url: "http://localhost:8091/graphql",
+            contentType: "application/json",
+            timeout: 15000,
+            data: JSON.stringify({
+                query: query,
+                variables: {
+                    getDetalleCarritosByIdCarritoId: idCarrito
+                }
+            })
+        });
+        return response.data.getDetalleCarritosByIdCarrito;
+    } catch (error) {
+        console.error("Error al obtener los productos del carrito:", error);
+    }
+}
+function creaCardCarrito(producto, cantidad, detalleCarrito, precio){
+    return `
+    <div class="card mb-3" data-id="${detalleCarrito}">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="${producto.foto}" class="img-fluid rounded-start" alt="${producto.nombre}">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${producto.nombre}</h5>
+                    <p id="precio-${producto.id}" class="card-text text-muted" value="${precio}">$${precio*cantidad}</p>
+
+                    <!-- Contador de cantidad de productos -->
+                    <div class="d-flex align-items-center mt-2">
+                        <button class="btn btn-outline-secondary btn-sm" onclick="cambiarDetalleCarrito('${detalleCarrito}', '${producto.id}', -1)">-</button>
+                        <span id="cantidad-${detalleCarrito}" class="mx-2">${cantidad}</span>
+                        <button class="btn btn-outline-secondary btn-sm" onclick="cambiarDetalleCarrito('${detalleCarrito}', '${producto.id}',  1)">+</button>
+                    </div>
+
+                    <button class="btn btn-danger btn-sm mt-3" onclick="eliminaDetalleCarrito('${detalleCarrito}')">Eliminar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+}
+
+// Traslación de los resolvers al Front-End
+function addDetalleCarrito(idCarrito, idProducto, cantidad){
+    const mutation =`
+    mutation mut($input: DetalleCarritoInput){
+        addDetalleCarrito(input: $input) {
+            id
+            carrito
+            producto
+            cantidad
+        }
+    }
+    `;
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8091/graphql",
+        contentType: "application/json",
+        timeout: 15000,
+        data: JSON.stringify({
+            query: mutation,
+            variables: {
+                input:{
+                    carrito: idCarrito,
+                    producto: idProducto,
+                    cantidad: cantidad
+                }
+            }
+        })
+    })
+}
+function updDetalleCarrito(idDetalle, inputDetalle){
+    const mutation = `
+        mutation mut($updDetalleCarritoId: ID!, $input: DetalleCarritoInput){
+            updDetalleCarrito(id: $updDetalleCarritoId, input: $input) {
+                id
+                carrito
+                producto
+                cantidad
+            }
+        }
+    `;
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8091/graphql",
+        contentType: "application/json",
+        timeout: 15000,
+        data: JSON.stringify({
+            query: mutation,
+            variables: {
+                updDetalleCarritoId: idDetalle,
+                input: inputDetalle
+            }
+        })
+    })
+}
+function delDetalleCarrito(idDetalle){
+    const mutation = `
+        mutation DelDetalleCarrito($delDetalleCarritoId: ID!){
+            delDetalleCarrito(id: $delDetalleCarritoId) {
+                message
+            }
+        }
+    `;
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8091/graphql",
+        contentType: "application/json",
+        timeout: 15000,
+        data: JSON.stringify({
+            query: mutation,
+            variables: {
+                delDetalleCarritoId: idDetalle
+            }
+        })
+    })
+}
+
+// Aplicación de resolvers en el contexto del Front-End
+async function agregarDetalleCarrito(){
+    let idCarrito = document.getElementById('carrito').getAttribute('value');
+    let idProducto = document.getElementById('productModal').getAttribute('value');
+    let nuevaCantidad = parseInt(document.getElementById('quantity').textContent);
+    let anteriorCantidad;
+    let productosCarrito = await getProductosByIdCarrito(idCarrito);
+    let idDetalle;
+    let esta = false;
+    var i = 0;
+    while (i < productosCarrito.length && !esta){
+        if (productosCarrito[i].producto == idProducto){
+            esta = true;
+            idDetalle = productosCarrito[i].id;
+            anteriorCantidad = productosCarrito[i].cantidad;
+        }
+        i++;
+    }
+
+    if (esta){
+        inputDetalle = {
+            carrito: idCarrito,
+            producto: idProducto,
+            cantidad: anteriorCantidad + nuevaCantidad
+        }
+        updDetalleCarrito(idDetalle, inputDetalle);
+    } else{
+        addDetalleCarrito(idCarrito, idProducto, nuevaCantidad);
+    }
+}
+async function cambiarDetalleCarrito(detalleCarrito, idProducto, masmenos){
+    let idCarrito = document.getElementById('carrito').getAttribute('value');
+    let cantidadElemento = document.getElementById('cantidad-' + detalleCarrito);
+    let precioElemento = document.getElementById('precio-' + idProducto)
+    let antiguaCantidad = parseInt(cantidadElemento.textContent);
+    let nuevaCantidad = antiguaCantidad + masmenos
+    if (nuevaCantidad > 0){
+        cantidadElemento.textContent = nuevaCantidad;
+        precioElemento.textContent = '$' + nuevaCantidad * parseInt(precioElemento.getAttribute('value'));
+        inputDetalle = {
+            carrito: idCarrito,
+            producto: idProducto,
+            cantidad: nuevaCantidad
+        }
+        updDetalleCarrito(detalleCarrito, inputDetalle);
+    }
+    
+}
+async function eliminaDetalleCarrito(detalleCarrito){
+    //TODO: taría weno un "¿Estás seguro de querer eliminar este producto, guapo?"
+    let cardCarrito = document.querySelector('#carrito .offcanvas-body').querySelector('div[data-id="' + detalleCarrito + '"]');
+    cardCarrito.remove();
+    delDetalleCarrito(detalleCarrito);
+}
---
+++ Front-End/js/modalProducto.js
@@ -0,0 +1,75 @@
+// Cositas para el contador en el modal
+let quantity = 1;
+function updateQuantity(change) {
+    // Actualiza la cantidad, asegurándose de que no sea menor a 1
+    quantity = Math.max(1, quantity + change);
+    // Actualiza el contador en el HTML
+    document.getElementById("quantity").textContent = quantity;
+    let precio = parseInt(document.getElementById('productModalPrecio').getAttribute('value'));
+    document.getElementById('productModalPrecio').textContent = '$' + quantity * precio;
+}
+async function actualizarModal(value) {
+    let query = `
+    query GetProductoById($getProductoByIdId: ID!) {
+        getProductoById(id: $getProductoByIdId) {
+            nombre
+            descripcion
+            foto
+            id
+        }
+    }
+    `;
+    try{
+        let responseProducto = await $.ajax({
+            type: "POST",
+            url: "http://localhost:8091/graphql",
+            contentType: "application/json",
+            timeout: 15000,
+            data: JSON.stringify({
+                query: query,
+                variables: {
+                    getProductoByIdId: value
+                }
+            })
+        })
+        let responsePrecio = await GetUltimoPrecioHistoricoByIdProducto(responseProducto.data.getProductoById.id);
+        document.getElementById('productModalLabel').textContent = responseProducto.data.getProductoById.nombre;
+        document.getElementById('productModalImage').innerHTML = '<img src="' + responseProducto.data.getProductoById.foto + '" alt="Imagen del Producto" class="img-fluid rounded">';
+        document.getElementById('productModalDesc').textContent = responseProducto.data.getProductoById.descripcion;
+        document.getElementById('productModal').setAttribute('value', responseProducto.data.getProductoById.id);
+        document.getElementById('productModalPrecio').textContent = '$' + responsePrecio.precio;
+        document.getElementById('productModalPrecio').setAttribute('value', responsePrecio.precio);
+    } catch(error) {
+        console.error("Error al modificar el modal del producto:", error);
+    }
+}
+
+function GetUltimoPrecioHistoricoByIdProducto(idProducto){
+    let query = `
+    query miQuery($id: String){
+        getUltimoPrecioHistoricoByIdProducto(id: $id){
+            id
+            precio
+            fecha
+        }
+    }
+    `;
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:8091/graphql",
+            contentType: "application/json",
+            timeout: 15000,
+            data: JSON.stringify({
+                query: query,
+                variables: {
+                    id: idProducto
+                }
+            }),
+            success: function(response){
+                resolve(response.data.getUltimoPrecioHistoricoByIdProducto);
+            }
+        });
+    });
+
+}
